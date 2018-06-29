a8563503a9e56c5267585986c540fa87a8e012e9
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-06-28 20:47:07 -0500

    Added RESTful patch route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,17 @@ class ProductsController < ApplicationController
 
   def show
     output = params["id"]
-    @beer = Product.find_by(id: output)
+    @beer = Product.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @beer = Product.find_by(id: params[:id])
+    @beer.name = params[:name] || @beer.name
+    @beer.price = params[:price] || @beer.price
+    @beer.image_url = params[:image_url] || @beer.image_url
+    @beer.description = params[:description] || @beer.description
+    @beer.save
     render "show.json.jbuilder"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/beers" => "products#index"
   post "/beers" => "products#create"
   get "/beers/:id" => "products#show"
+  patch "/beers/:id" => "products#update"
 
 end
