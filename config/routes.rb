22e2b82eb852b988b6cccebd3347166a9c48a68f
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-06-28 19:40:48 -0500

    deleted extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   get "/beers" => "products#index"
-  get "/ipa_beer" => "products#ipa_beer_for_sale"
-  get "/DIPA_beer" => "products#double_ipa_beer_for_sale"
-  get "/RedAle_beer" => "products#red_ale_beer_for_sale"
-  get "/all_products_url" => "products#all_products_model"
   get "/beers/:id" => "products#show"
 end
