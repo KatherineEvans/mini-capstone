50bcf761d4d3f8e6827085d7edfcd7e8c0390c2f
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-06-28 20:22:19 -0500

    Added create route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,17 @@ class ProductsController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @beer = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"]
+      )
+    @beer.save
+    render "show.json.jbuilder"
+  end
+
   def show
     output = params["id"]
     @beer = Product.find_by(id: output)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/beers" => "products#index"
+  post "/beers" => "products#create"
   get "/beers/:id" => "products#show"
+
 end
