5ebdbaa992f6fed4b8e7c5e230c992800b986c0e
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-07-10 20:33:46 -0500

    added authorization to orders controller index & create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,24 +4,31 @@ class OrdersController < ApplicationController
     if current_user
       @orders = Order.where(user_id: current_user.id)
       render "index.json.jbuilder"
+    else
+      render json: [], status: :unauthorized
     end
   end
 
   def create
-    @beer = Product.find_by(id: params[:product_id])
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      subtotal: @beer.price * params["quantity"].to_i,
-      tax: @beer.tax * params["quantity"].to_i,
-      total: @beer.total * params["quantity"].to_i
-      )
+    if current_user
+      @beer = Product.find_by(id: params[:product_id])
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        subtotal: @beer.price * params["quantity"].to_i,
+        tax: @beer.tax * params["quantity"].to_i,
+        total: @beer.total * params["quantity"].to_i
+        )
 
-    if @order.save
-      render json: {message: "Order Saved"}
-    else 
-      render json: {message: "YOU SUCK"}
+      if @order.save
+        render json: {message: "Order Saved"}
+      else 
+        render json: {message: "YOU SUCK"}
+      end
+
+    else
+      render json: [], status: :unauthorized
     end
   end
 
