e062d7ce070748d6f7c58b7883156a4d58451bb6
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-07-10 20:38:18 -0500

    dry authentication code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
 
   def index
-    if current_user
-      @orders = Order.where(user_id: current_user.id)
-      render "index.json.jbuilder"
-    else
-      render json: [], status: :unauthorized
-    end
+    @orders = current_user.orders
+    render "index.json.jbuilder"
   end
 
   def create
-    if current_user
-      @beer = Product.find_by(id: params[:product_id])
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params["product_id"],
-        quantity: params["quantity"],
-        subtotal: @beer.price * params["quantity"].to_i,
-        tax: @beer.tax * params["quantity"].to_i,
-        total: @beer.total * params["quantity"].to_i
-        )
+    @beer = Product.find_by(id: params[:product_id])
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: @beer.price * params["quantity"].to_i,
+      tax: @beer.tax * params["quantity"].to_i,
+      total: @beer.total * params["quantity"].to_i
+      )
 
-      if @order.save
-        render json: {message: "Order Saved"}
-      else 
-        render json: {message: "YOU SUCK"}
-      end
-
-    else
-      render json: [], status: :unauthorized
+    if @order.save
+      render json: {message: "Order Saved"}
+    else 
+      render json: {message: "YOU SUCK"}
     end
   end
 
